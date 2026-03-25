609871c774763ead18c1da8f0020b346656c331b
mugi-uiux <cy22262@shibaura-it.ac.jp>
2026-03-25 12:22:56 +0900

    style(layout): Remove Footer component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Header } from './Header';
-import { Footer } from './Footer';
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -13,7 +12,6 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
             <main className="container mx-auto px-4 py-8 flex-1">
                 {children}
             </main>
-            <Footer />
         </div>
     );
 };
